cdecb5dac61c411700601e18bbc6d6a6b183c881
marco <marco@webguerilla.net>
2015-04-27 22:26:53 +0200

    added copyright

--- mod_sitemapgenerator/src/module/mod_sitemapgenerator.php
+++ mod_sitemapgenerator/src/module/mod_sitemapgenerator.php
@@ -1,4 +1,9 @@
 <?php
-defined('_JEXEC') or die;
+/*
+ * @copyright  Copyright (C) 2015 Marco Beierer. All rights reserved.
+ * @license    http://www.gnu.org/licenses/agpl-3.0.html GNU/AGPL
+ */
+
+defined('_JEXEC') or die();
 
 require JModuleHelper::getLayoutPath('mod_sitemapgenerator', $params->get('layout', 'default'));
--- mod_sitemapgenerator/src/module/tmpl/default.php
+++ mod_sitemapgenerator/src/module/tmpl/default.php
@@ -1,5 +1,10 @@
 <?php
-defined('_JEXEC') or die;
+/*
+ * @copyright  Copyright (C) 2015 Marco Beierer. All rights reserved.
+ * @license    http://www.gnu.org/licenses/agpl-3.0.html GNU/AGPL
+ */
+
+defined('_JEXEC') or die();
 
 JHtml::_('behavior.modal');
 ?>
